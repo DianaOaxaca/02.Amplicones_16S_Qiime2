a6ee5cd2517a9e6ea5a04ed26b6b11c4b57db5d0
Diana Hernández-Oaxaca <hoaxacadiana@gmail.com>
2023-09-01 19:38:27 -0600

    Update 00.qc_preprocessing.sh

--- src/00.qc_preprocessing.sh
+++ src/00.qc_preprocessing.sh
@@ -6,7 +6,7 @@ declare -a FASTQs=("$FASTQ")
 for FILE in ${FASTQs[@]}; do
 	echo $FILE
 	#FASTQCline='fastqc data/'$FILE'_1.fastq.gz  data/'$FILE'_2.fastq.gz -o results/00.fastqc/'
-	TRIMline='trim_galore --fastqc --illumina  --trim-n --gzip  --paired data/'$FILE'_1.fastq.gz  data/'$FILE'_2.fastq.gz -o results/00.trimgalore/'$FILE'_trimgalore'
+	TRIMline='trim_galore --fastqc --trim-n --gzip  --paired data/'$FILE'_1.fastq.gz  data/'$FILE'_2.fastq.gz -o results/00.trimgalore/'$FILE'_trimgalore'
 	echo -e $FASTQCline"\n"$TRIMline 
 	$TRIMline 
 done
